de627cafd7f407f5cd20e241039b2a579bed8620
Ritik Gupta <97462674+codewithritik@users.noreply.github.com>
2023-03-19 13:38:26 +0530

    git added

---
+++ src/apiKeys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  key: "a9f97e4af8263f6c0367aa8155832086",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
---
+++ src/currentLocation.js
@@ -0,0 +1,202 @@
+import React from "react";
+import apiKeys from "./apiKeys";
+import Clock from "react-live-clock";
+import Forcast from "./forcast";
+import loader from "./images/WeatherIcons.gif";
+import ReactAnimatedWeather from "react-animated-weather";
+const dateBuilder = (d) => {
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day}, ${date} ${month} ${year}`;
+};
+const defaults = {
+  color: "white",
+  size: 112,
+  animate: true,
+};
+class Weather extends React.Component {
+  state = {
+    lat: undefined,
+    lon: undefined,
+    errorMessage: undefined,
+    temperatureC: undefined,
+    temperatureF: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    icon: "CLEAR_DAY",
+    sunrise: undefined,
+    sunset: undefined,
+    errorMsg: undefined,
+    citybackground:""
+  };
+
+  componentDidMount() {
+    if (navigator.geolocation) {
+      this.getPosition()
+        //If user allow location service then will fetch data & send it to get-weather function.
+        .then((position) => {
+          this.getWeather(position.coords.latitude, position.coords.longitude);
+        })
+        .catch((err) => {
+          //If user denied location service then standard location weather will le shown on basis of latitude & latitude.
+          this.getWeather(19.076090 , 19.076090);
+          alert(
+            "You have disabled location service. Allow 'This APP' to access your location. Your current location will be used for calculating Real time weather."
+          );
+        });
+    } else {
+      alert("Geolocation not available");
+    }
+
+    this.timerID = setInterval(
+      () => this.getWeather(this.state.lat, this.state.lon),
+      600000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+
+  getPosition = (options) => {
+    return new Promise(function (resolve, reject) {
+      navigator.geolocation.getCurrentPosition(resolve, reject, options);
+    });
+  };
+  
+  getWeather = async (lat, lon) => {
+    const api_call = await fetch(
+      `${apiKeys.base}weather?lat=${lat}&lon=${lon}&units=metric&APPID=${apiKeys.key}`
+    );
+    const data = await api_call.json();
+    this.setState({
+      lat: lat,
+      lon: lon,
+      city: data.name,
+      temperatureC: Math.round(data.main.temp),
+      temperatureF: Math.round(data.main.temp * 1.8 + 32),
+      humidity: data.main.humidity,
+      main: data.weather[0].main,
+      country: data.sys.country,
+    });
+    switch (this.state.main) {
+      case "Haze":
+        this.setState({ icon: "CLOUDY", citybackground:"https://images.unsplash.com/photo-1592210454359-9043f067919b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" });
+        break;
+      case "Clouds":
+        this.setState({ icon: "CLOUDY",citybackground:"https://images.unsplash.com/photo-1534088568595-a066f410bcda?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=451&q=80" });
+        break;
+      case "Rain":
+        this.setState({ icon: "RAIN", citybackground:"https://images.unsplash.com/photo-1558409057-bbe679023136?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1037&q=80" });
+        break;
+      case "Snow":
+        this.setState({ icon: "SNOW", citybackground:"https://images.unsplash.com/photo-1514632595-4944383f2737?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" });
+        break;
+      case "Dust":
+        this.setState({ icon: "WIND", citybackground:"https://images.unsplash.com/photo-1456356627738-3a96db6e2e33?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" });
+        break;
+      case "Drizzle":
+        this.setState({ icon: "SLEET", citybackground:"https://images.unsplash.com/photo-1508697014387-db70aad34f4d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80" });
+        break;
+      case "Fog":
+        this.setState({ icon: "FOG", citybackground:"https://images.unsplash.com/photo-1485236715568-ddc5ee6ca227?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" });
+        break;
+      case "Smoke":
+        this.setState({ icon: "FOG", citybackground:"https://images.unsplash.com/photo-1582655741936-dd947f20bb80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" });
+        break;
+      case "Tornado":
+        this.setState({ icon: "WIND", citybackground:"https://images.unsplash.com/photo-1508697014387-db70aad34f4d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80" });
+        break;
+        case "Thunderstorm":
+        this.setState({ icon: "PARTLY_CLOUDY_NIGHT", citybackground:"https://images.unsplash.com/photo-1508697014387-db70aad34f4d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80" });
+        break;
+      default:
+        this.setState({ icon: "CLEAR_DAY", citybackground:"https://keyassets.timeincuk.net/inspirewp/live/wp-content/uploads/sites/8/2020/10/GettyImages-1209643252.jpg" });
+    }
+  };
+
+  render() {
+    if (this.state.temperatureC) {
+      return (
+        <React.Fragment>
+          <div className="city" style={{background:`${this.state.citybackground}`}}>
+            <div className="title">
+              <h2>{this.state.city}</h2>
+              <h3>{this.state.country}</h3>
+            </div>
+            <div className="mb-icon">
+              {" "}
+              <ReactAnimatedWeather
+                icon={this.state.icon}
+                color={defaults.color}
+                size={defaults.size}
+                animate={defaults.animate}
+              />
+              <p>{this.state.main}</p>
+            </div>
+            <div className="date-time">
+              <div className="dmy">
+                <div id="txt"></div>
+                <div className="current-time">
+                  <Clock format="HH:mm:ss" interval={1000} ticking={true} />
+                </div>
+                <div className="current-date">{dateBuilder(new Date())}</div>
+              </div>
+              <div className="temperature">
+                <p>
+                  {this.state.temperatureC}°<span>C</span>
+                </p>
+              </div>
+            </div>
+          </div>
+          <Forcast icon={this.state.icon} weather={this.state.main} />
+        </React.Fragment>
+      );
+    } else {
+      return (
+        <React.Fragment>
+          <img src={loader} style={{ width: "50%", WebkitUserDrag: "none" }} />
+          <h3 style={{ color: "white", fontSize: "22px", fontWeight: "600" }}>
+            Detecting your location
+          </h3>
+          <h3 style={{ color: "white", marginTop: "10px" }}>
+            Your current location wil be displayed on the App <br></br> & used
+            for calculating Real time weather.
+          </h3>
+        </React.Fragment>
+      );
+    }
+  }
+}
+
+export default Weather;
